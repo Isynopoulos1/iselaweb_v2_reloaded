463343b1898f23c0dfec37444d34e7b650643c12
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-04-30 22:21:25 +0200

    adding the text area in the form section

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,19 +1,16 @@
 import React from "react";
-
+import { ContactContainer, Greetings, FormContainer, InputStyle, TextArea } from "./Contact.styles";
 //IMPORT STYLES
-import { ContactContainer, Greetings, formContainer } from "./Contact.styles";
 
 const Contact = () => {
   return (
     <ContactContainer>
       <Greetings>Let's talk!</Greetings>
-      <formContainer>
-        <input type="form" placeholder="Name" />
-        <input type="text" placeholder="Mail" />
-        <textarea name="Message" id="" cols="30" rows="10">
-          Message
-        </textarea>
-      </formContainer>
+      <FormContainer>
+        <InputStyle type="form" placeholder="Name" />
+        <InputStyle type="text" placeholder="Mail" />
+        <TextArea name="Message" id="textarea" cols="30" rows="10" defaultValue="Initial value" />
+      </FormContainer>
     </ContactContainer>
   );
 };
--- src/components/pages/contact/Contact.styles.js
+++ src/components/pages/contact/Contact.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { fonts } from "../../../styles/index";
 
 export const ContactContainer = styled.div`
-  width: 100;
+  width: 100%;
   justify-content: center;
   align-items: center;
   position: relative;
@@ -19,8 +19,14 @@ export const Greetings = styled.h1`
   font-family: ${fonts.font1};
 `;
 
-export const formContainer = styled.div`
+export const FormContainer = styled.form`
   width: 100%;
   display: flex;
   flex-direction: column;
+`;
+export const InputStyle = styled.input`
+  width: 100%;
+`;
+export const TextArea = styled.textarea`
+  width: 100%;
 `;
